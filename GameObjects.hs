02d50992e5f5c53e97034db8e1587e32f0011cfa
Michal Luszczek <michluszcz@gmail.com>
2023-12-16 20:11:09 +0100

    adeed objects to workshop

--- GameObjects.hs
+++ GameObjects.hs
@@ -46,20 +46,50 @@ workshop =
     { roomName = "workshop",
       roomDescription = "The workshop is where most engineering on the station happens.",
       roomObjects =
-        [alien_mass],
+        [alien_mass, engineering_chief_office_door, toolbox, workshop_window, small_fire, table],
       roomExits = Map.fromList [(South, initialRoom)]
     }
 
 
+
+-- TODO: add destroy handling
 alien_mass :: Object
 alien_mass = Object "alien_mass" "A strange black mass near the *workshop_window* blocks the path south. It pulsates slightly, as if breathing.\n
   Underneath it you see one of your collegues being slowly absorbed by what you assume to be some kind of alien intruder. \n
     A familiar smell of fuel fumes seems to be eminating from the creature.\n
     It migth be flammable" (Map.fromList [("pickable", False)])
 
+--TODO: add opening with engineering_chief_access_card
+engineering_chief_office_door :: Object
+engineering_chief_office_door = 
+  Object "engineering_chief_office_door" 
+  "The door to the chief's office, usually locked by an access card"
+  (Map.fromList [("pickable", False), ("openable", False)])
 
 
+-- TODO: Add open handling for toolbox
+toolbox :: Object
+toolbox = 
+  Object "toolbox" "Standard-issue toolbox. It's unlocked" (Map.fromList [("pickable", False), ("openable", True)])
+
+ -- TODO: HANDLE destroying
+workshop_window :: Object
+workshop_window = 
+  Object "workshop_window" "You look at the window and into space. You see pieces of debris coming from the ship as well as some strange black round objects you can't identify\n
+   Can be broken with enough force. Last time this happened 2 workers got sucked out into space."
+   (Map.fromList [("pickable", False)])
+
+-- TODO handle lighting on fire
+small_fire :: Object
+small_fire = 
+  Object "small_fire" "A small electical fire seems to have broken out in the corner of the room"
+  (Map.fromList [("pickable", False)])
 
 
-
+-- TODO handle breaking table
+table :: Object
+table = 
+  Object "table" "An old wooden table. One of its legs seems to be barely holding on. \n
+  You might be able to detach it if you had the proper tool."
+  (Map.fromList [("pickable", False)])
 
